3802349c69fc9f7cb883778c2af9ce57f199b35b
ashton <nealashtonheston@gmail.com>
2015-05-10 20:27:30 -0700

    Inital Commit

---
+++ src/org/dojoweb/TestServlet.java
@@ -0,0 +1,48 @@
+package org.dojoweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name1");
+		String r = "";
+		for (int i = name.length()-1; i >= 0 ; i--) {
+			r+=name.charAt(i);
+		}
+
+		PrintWriter pw = response.getWriter();
+		pw.print("<html><body><div style='color: red;'>" + r  + "</div></body></html>");
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
